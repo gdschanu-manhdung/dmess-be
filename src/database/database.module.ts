614068b128d11fad935fa91c0862f79fa32af299
Maduro29 <manhdungtran.vnuuet@gmail.com>
2024-02-23 16:42:41 +0700

    feat: connect neon.tech db

---
+++ src/database/database.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Conversation } from './typeorm/entities/Conversation';
+import { Friend } from './typeorm/entities/Friend';
+import { Member } from './typeorm/entities/Member';
+import { Message } from './typeorm/entities/Message';
+import { Reaction } from './typeorm/entities/Reaction';
+import { User } from './typeorm/entities/User';
+
+@Module({
+    imports: [
+        ConfigModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('PGHOST'),
+                port: configService.get('PGPORT'),
+                username: configService.get('PGUSER'),
+                password: configService.get('PGPASSWORD'),
+                database: configService.get('PGDATABASE'),
+                entities: [
+                    Conversation,
+                    Friend,
+                    Member,
+                    Message,
+                    Reaction,
+                    User,
+                ],
+                synchronize: true,
+            }),
+        }),
+    ],
+})
+export class DatabaseModule {}
